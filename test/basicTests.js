00a69c9c26de99ac8890a97a9735d398206c604d
billey <guillaume.billey@inist.fr>
2014-03-17 14:25:36 +0100

    First commit

---
+++ test/basicTests.js
@@ -0,0 +1,12 @@
+describe("Basic tests", function() {
+
+    it("Un panier vide ne coûte rien du tout", function() {
+        expect(priceOf([])).toBe(0);
+    });
+
+    it("Un panier avec 1 bière coûte 5 €", function() {
+       pending();
+    });
+
+
+});
