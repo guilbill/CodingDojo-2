57c4f6a5a77cdb9608f171b02d2d21a097a18026
codingDojo1 <guillaume.billey@inist.fr>
2014-03-17 15:39:15 +0100

    fix every

--- beer.js
+++ beer.js
@@ -3,14 +3,15 @@
 // 5 types de bières
 var beers = ["Guinness", "Chimay", "Loroyse", "Karmeliet", "Kriek"];
 
-function priceOf(beers) {
+function priceOf(panier) {
 	// Known beer?
 	var isAKnownBeer = function(beer, index) {
-		return beers.indexOf(beer) != -1;
+		console.log(beer);
+		return (beers.indexOf(beer) != -1);
 	};
-	var goodBeer = beers.every(isAKnownBeer);
-	console.log(goodBeer);
+	
+	var goodBeer = panier.every(isAKnownBeer);
 	if (!goodBeer) throw new Exception("This beer is not good ;)");
 
-	return beers.length * 5;
+	return panier.length * 5;
 }
