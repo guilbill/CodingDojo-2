22fca5d10d5743011646743429d7191c44822233
codingDojo1 <guillaume.billey@inist.fr>
2014-03-17 15:45:58 +0100

    test deux bieres

--- beer.js
+++ beer.js
@@ -6,12 +6,15 @@ var beers = ["Guinness", "Chimay", "Loroyse", "Karmeliet", "Kriek"];
 function priceOf(panier) {
 	// Known beer?
 	var isAKnownBeer = function(beer, index) {
-		console.log(beer);
 		return (beers.indexOf(beer) != -1);
 	};
-	
+
 	var goodBeer = panier.every(isAKnownBeer);
 	if (!goodBeer) throw new Exception("This beer is not good ;)");
+
+	if ((panier.length > 1) && (panier[0] != panier[1])) {
+
+	}
 
 	return panier.length * 5;
 }
--- test/basicTests.js
+++ test/basicTests.js
@@ -9,8 +9,11 @@ describe("Basic tests", function() {
     });
 
     it("un panier contenant X bières du même type coûtera X * 5 euros", function(){
-    	pending();
+    	expect(priceOf(["Guinness", "Guinness", "Guinness"])).toBe(15)
+    });
 
+    it("5% de réduc si deux bières différentes", function(){
+    	expect(priceOf(["Guinness", "Chimay"])).toBe(9.5)
     });
 
     it("Il n'y a que 5 types de bières", function() {
